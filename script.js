6760f1e4b1801ddab247465e5a4aee7790374d18
Tessag23 <126432469+Tessag23@users.noreply.github.com>
2023-06-18 00:11:32 +0530

    created

---
+++ script.js
@@ -0,0 +1,10 @@
+function temp(){
+    var c=document.getElementById("c").value;
+    var f=(c*9/5)+32;
+    document.getElementById("f").value=f;
+}
+function wt(){
+    var k=document.getElementById("kg").value;
+    var p=k*2.2;
+    document.getElementById("p").value=p;
+}
